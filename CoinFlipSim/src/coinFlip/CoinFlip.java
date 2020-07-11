d4f25d1de4e31adc4b6938e23a6bc80b3efae0b6
Gbemi Akerele <gbemim@my.yorku.ca>
2020-07-10 21:17:17 -0400

    needs debugging but all the methods are included

--- CoinFlipSim/src/coinFlip/CoinFlip.java
+++ CoinFlipSim/src/coinFlip/CoinFlip.java
@@ -1,5 +1,7 @@
 package coinFlip;
 
+import java.util.Scanner;
+
 public class CoinFlip {
 	private char headOrTail;
 	private int numOfFlip;
@@ -17,52 +19,48 @@ public class CoinFlip {
 	}
 	
 	
-	public int numOfRuns(int n) {
-		numOfFlip = n;
-		int countH = 0;
-		int countT = 0;
-		for (int i = 1; i <= n; i++) {
+	public int numOfHead() {
+		int countH;
+		System.out.println(this.numOfFlip);
+		countH = 0;
+		for (int i = 1; i <= this.numOfFlip; i++) {
 			if (this.headOrTail == 'H') {
-				countH =+ 1;
+				countH = countH + 1;
 			}
-			
-		}
+		}	
+		System.out.println(countH);
+		return countH;
+	}
 		
-		
-		
-		
+	public int numOfTail() {
+		int countT = 0;
+		for (int i = 1; i <= this.numOfFlip; i++) {
+			if (this.headOrTail == 'T') {
+				countT =+ 1;
+			}	
+		}	
+		return countT;
 	}
 	
 	
+	@Override
+	public String toString() {
+		return "The number of heads: " + numOfHead() + "\n"
+				+ "The number of tails: " + numOfTail();
+	}
+
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 	
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("How many times would you like to flip the coin> ");
+		int flip = input.nextInt();
+		
+		CoinFlip newCase = new CoinFlip(flip);
+		System.out.print(newCase.numOfHead());
 	}
 
 }
