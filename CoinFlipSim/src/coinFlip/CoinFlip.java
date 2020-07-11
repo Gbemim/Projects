c954237968ddd7d192bd0c2c49612160bf0bf8d3
Gbemi Akerele <gbemim@my.yorku.ca>
2020-07-10 20:47:46 -0400

    2methods added headOfTails and numOfRuns

--- CoinFlipSim/src/coinFlip/CoinFlip.java
+++ CoinFlipSim/src/coinFlip/CoinFlip.java
@@ -1,7 +1,65 @@
 package coinFlip;
 
 public class CoinFlip {
-
+	private char headOrTail;
+	private int numOfFlip;
+	
+	public CoinFlip(int n) {
+		numOfFlip = n;
+	}
+	
+	public void headOrTail() {
+		double oneOrZero = Math.round(Math.random());
+		if (oneOrZero == 1.0) {
+			this.headOrTail = 'H';
+		} 
+		else this.headOrTail = 'T';
+	}
+	
+	
+	public int numOfRuns(int n) {
+		numOfFlip = n;
+		int countH = 0;
+		int countT = 0;
+		for (int i = 1; i <= n; i++) {
+			if (this.headOrTail == 'H') {
+				countH =+ 1;
+			}
+			
+		}
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
